33accc83d55416f544878c21b8aaf26a79d453d4
cyberuzo <cyberuzo@yahoo.com>
2020-08-31 07:16:09 +0100

    Configuration to deploy to heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GridPagingSampleComponent } from './grid/grid-paging-sample/grid-paging-sample.component';
+import { GridFilteringSampleComponent } from './grid/grid-filtering-sample/grid-filtering-sample.component';
+import { GridSortingIndicatorsComponent } from './grid/grid-sorting-indicators/grid-sorting-indicators.component';
+import { GridEditingSampleComponent } from './grid/grid-editing-sample/grid-editing-sample.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: GridPagingSampleComponent
+  },
+  {
+    path: 'paging',
+    pathMatch: 'full',
+    component: GridPagingSampleComponent
+  },
+  {
+    path: 'filtering',
+    pathMatch: 'full',
+    component: GridFilteringSampleComponent
+  },
+  {
+    path: 'sorting',
+    pathMatch: 'full',
+    component: GridSortingIndicatorsComponent
+  },
+  {
+    path: 'editing',
+    pathMatch: 'full',
+    component: GridEditingSampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,70 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridPagingSampleComponent } from './grid/grid-paging-sample/grid-paging-sample.component';
+import { FormsModule } from "@angular/forms";
+
+import { HttpClientModule } from "@angular/common/http";
+import { 
+	IgxAvatarModule,
+	IgxBadgeModule,
+	IgxButtonModule,
+	IgxGridModule,
+	IgxIconModule,
+	IgxInputGroupModule,
+	IgxProgressBarModule,
+	IgxRippleModule,
+	IgxSwitchModule,
+	IgxCheckboxModule,
+	IgxDatePickerModule,
+	IgxDialogModule,
+	IgxComboModule,
+	IgxToastModule,
+ } from "igniteui-angular";
+ import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+import { GridFilteringSampleComponent } from './grid/grid-filtering-sample/grid-filtering-sample.component';
+import { GridSortingIndicatorsComponent } from './grid/grid-sorting-indicators/grid-sorting-indicators.component';
+import { GridEditingSampleComponent } from './grid/grid-editing-sample/grid-editing-sample.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GridPagingSampleComponent,
+    GridFilteringSampleComponent,
+    GridSortingIndicatorsComponent,
+    GridEditingSampleComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		IgxPreventDocumentScrollModule,
+		IgxAvatarModule,
+		IgxBadgeModule,
+		IgxButtonModule,
+		IgxGridModule,
+		IgxIconModule,
+		IgxInputGroupModule,
+		IgxProgressBarModule,
+		IgxRippleModule,
+		IgxSwitchModule,
+		HttpClientModule,
+		IgxCheckboxModule,
+		IgxDatePickerModule,
+		IgxDialogModule,
+		IgxToastModule,
+		IgxComboModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
